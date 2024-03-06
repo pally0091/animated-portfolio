4f9b5670c9e10adb6f831d1617ec3b389f82820d
Debashis Roy <goodquote39@hotmail.com>
2024-03-06 23:16:16 +0600

    trying  something

--- src/Components/Projects/Experience.jsx
+++ src/Components/Projects/Experience.jsx
@@ -6,15 +6,18 @@ const Experience = () => {
   return (
     <div>
       <Header>My Experiences</Header>
-      <div className="mt-10 w-[90%] me-0 ms-auto flex flex-col-reverse gap-5">
+      <div className="mt-10 w-[90%] me-0 ms-auto flex flex-col-reverse h-[500px] overflow-y-scroll gap-5">
         {experience.map((exp, index) => (
           <motion.div
             key={index}
-            className="py-4 px-8 bg-neutral-800 rounded-xl shadow-inner shadow-[#BB9864]"
-            initial={{ opacity: 0, x: -200 }}
+            className="py-4 px-8 rounded-xl shadow-inner shadow-[#BB9864]"
+            initial={{
+              opacity: 0,
+              scale: 0,
+            }}
             transition={{ duration: 0.6 }}
-            whileHover={{ x: -50 }}
-            whileInView={{ opacity: 1, x: 0 }}
+            whileHover={{ boxShadow: "inset 0px 0px 10px 10px white" }}
+            whileInView={{ opacity: 1, scale: 1 }}
           >
             <h3 className="text-xl font-semibold">{exp.name}</h3>
             <p className="mb-2 text-sm">{exp.company}</p>
